320e1049e945eb0f3e491e173164c7f84dfc1354
Alpha <alpha6662017@outlook.com>
2022-01-12 01:23:30 +0800

    確認帳密功能加強與模組化

--- check.py
+++ check.py
@@ -1,17 +1,23 @@
 import ujson
 
-def check(input_account,input_password):
-    with open('data/admin.json','r',encoding='utf-8') as f:
-        admin=ujson.load(f)
+def check(input_account,input_password,datatype):
+    flag=False
+    if datatype=='Admin':
+        data='data/admin.json'
+    elif datatype=='user':
+        data='data/user.json'
+    with open(data,'r',encoding='utf-8') as f:
+        database=ujson.load(f)
         f.close()
-    account=admin['account']
-    password=str(admin['password'])
-    if input_account==account and input_password==password:
-        return True
-    else:
-        return False
+    for item in database:
+        account=item['account'];
+        password=str(item['password'])
+        if input_account==account and input_password==password:
+            flag=True
+    return flag
+            
 
 if __name__=='__main__':
     acc=str(input("account:"))
     pw=str(input("password:"))
-    print(check(acc,pw))
+    print(check(acc,pw,"Admin"))
