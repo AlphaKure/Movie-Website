3b74d06249d8c04f63997b2d0b2ece7ee8bc0906
Alpha <alpha6662017@outlook.com>
2022-01-12 19:31:17 +0800

    [核心] 註冊功能完成

--- core.py
+++ core.py
@@ -1,6 +1,6 @@
 import flask
 from check import check
-from register import register
+from register import register_acc
 
 app=flask.Flask(__name__)
 
@@ -11,6 +11,53 @@ nowuser=""
 @app.route('/')
 def main():
     return flask.redirect(flask.url_for('Home'))
+
+@app.route('/home')
+def Home():
+    return flask.render_template('come_soon.html',islogin=islogin)
+
+@app.route('/login',methods=["POST","GET"])
+def login():
+    global islogin
+    global nowuser
+    if flask.request.method=='POST':
+        input_account=flask.request.values.get('Username')
+        input_password=flask.request.values.get('Password')
+        islogin=check(input_account,input_password,"user")
+        if islogin==True:
+            nowuser=input_account
+            return flask.redirect(flask.url_for('Home'))
+        else:
+            flask.flash('登入失敗 請確認帳號和密碼')
+    return flask.render_template('login.html',islogin=islogin)
+
+@app.route('/logout')
+def logout():
+    global islogin
+    global nowuser
+    nowuser=""
+    islogin=False
+    return flask.redirect(flask.url_for('Home'))
+
+@app.route('/register',methods=["POST","GET"])
+def register():
+    global islogin
+    if flask.request.method=='POST':
+        input_account=flask.request.values.get('account')
+        input_password=flask.request.values.get('password')
+        check_password=flask.request.values.get('c_password')
+        if input_password!=check_password:
+            flask.flash('密碼不一致 請再確認一次')
+            return flask.redirect(flask.url_for('register'))
+        else:
+            msg=register_acc(input_account,input_password)
+            if msg=="account is exist!":
+                flask.flash('帳號已存在 請換一組帳號')
+                return flask.redirect(flask.url_for('register'))
+            else:
+                flask.flash('註冊已完成 請重新登入')
+                return flask.redirect(flask.url_for('login'))
+    return flask.render_template('register.html',islogin=islogin)
 
 @app.route('/Adminlogin',methods=["POST","GET"])
 def Adminlogin():
@@ -38,33 +85,6 @@ def Dashboard():
         return flask.render_template('Dashboard.html')
     else:
         return "You don't have Authority!"
-
-@app.route('/login',methods=["POST","GET"])
-def login():
-    global islogin
-    global nowuser
-    if flask.request.method=='POST':
-        input_account=flask.request.values.get('Username')
-        input_password=flask.request.values.get('Password')
-        islogin=check(input_account,input_password,"user")
-        if islogin==True:
-            nowuser=input_account
-            return flask.redirect(flask.url_for('Home'))
-        else:
-            flask.flash('登入失敗 請確認帳號和密碼')
-    return flask.render_template('login.html',islogin=islogin)
-
-@app.route('/logout')
-def logout():
-    global islogin
-    global nowuser
-    nowuser=""
-    islogin=False
-    return flask.redirect(flask.url_for('Home'))
-
-@app.route('/home')
-def Home():
-    return flask.render_template('come_soon.html',islogin=islogin)
 
 if __name__ == '__main__':
     app.secret_key="Movie-website"
